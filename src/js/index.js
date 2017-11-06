c1dfb04d71488726debd73956f2dd6f49a3902ab
wongeun heo <heowongeun@gmail.com>
2017-11-06 12:40:22 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
+var $ = require('jQuery');
 (function () {
-  var Load_Image        = require('./modules/loader/Load_Image');
-  var LoadingAnimation  = require('./class/ui/LoadingAnimation');
-  var imgLoader         = new Load_Image();
-  imgLoader.load($('body'),new LoadingAnimation(init));
+
+  $(document).ready(function(){
+
+  });
+
+  function load(){
+  	var Load_Image        = require('./modules/loader/Load_Image');
+	  var LoadingAnimation  = require('./class/ui/LoadingAnimation');
+	  var imgLoader         = new Load_Image();
+	  // imgLoader.load($('body'),new LoadingAnimation(init));
+    
+  }
 
   function init(){
-    console.log(init);
+    
   }
+
+
 
 }).call(this);
