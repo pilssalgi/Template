7161283fe3c68f4dbfcc43de29b9cdec4a4f08ad
wongeun heo <heowongeun@gmail.com>
2019-02-28 16:23:43 +0900

    update youtubeplayer

--- src/assets/js/modules/media/YoutubePlayer.js
+++ src/assets/js/modules/media/YoutubePlayer.js
@@ -10,45 +10,47 @@
   </script>
 */
 
-var $ = require('jQuery');
+const assignIn = require('lodash/assignIn');
 (function(){
+
   var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  document.body.append(tag);
 
+  // window.onYouTubeIframeAPIReady = () => {
+  //   this.setup();
+  //   window.onYouTubeIframeAPIReady = null
+  // }
   
-  var YoutubePlayer= function(youtubeID,domID,width,height,ready){
+  var YoutubePlayer = function(youtubeID,domID,width,height,ready){
     var _this       = this;
     var initialized = false,
         isReady     = false;
 
     this.player;
-    this.init = function(){
+    this.config = {
+      height: '640',
+      width: '480',
+      videoId: '',
+      playerVars:{},
+      events:{
+        onReady: (e)=>{this.onPlayerReady(e)},
+        onStateChange: (e)=>{this.onPlayerStateChange(e)}
+      }
+    }
+
+    assignIn(this.config,option);
+
+    function setup(){
       if(initialized)return;
       initialized = true;
-      this.player = new YT.Player(domID, {
-        height: height,
-        width: width,
-        videoId: youtubeID,
-        events: {
-          'onReady': _this.onPlayerReady,
-          'onStateChange': _this.onPlayerStateChange
-        }
-      });
-
-      return this;
+      this.player = new YT.Player(videoContainerID, this.config);
     }
 
     this.onPlayerReady = function(e){
-      isReady = true;
-      if(ready)ready(e);
-      // _this.player.stopVideo();
-
     }
 
     this.onPlayerStateChange = function(e){
-      
     }
 
     this.play = function(){
@@ -60,11 +62,11 @@ var $ = require('jQuery');
     }
 
     this.stop = function(){
-      if(!isReady)return;
       this.player.stopVideo();
     }
 
-    return this.init();
+    setup.call(this);
+    return this;
   };
   module.exports = YoutubePlayer;
 }).call(this);
